653cac019d7a86ccc9baacf25181382854d4078f
weilei <weilei@tiantonglaw.com>
2015-03-01 09:18:29 +0800

    init project

---
+++ src/main/java/org/radium/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.radium;
+
+/**
+ * Created by weilei on 15/3/1.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
