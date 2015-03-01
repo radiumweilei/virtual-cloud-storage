79085ade3b1e19fac8670f81f91354908474c9e4
weilei <weilei@tiantonglaw.com>
2015-03-01 09:23:38 +0800

    build directory structure

---
+++ src/test/java/org/radium/TestHelloWorld.java
@@ -0,0 +1,10 @@
+package org.radium;
+
+/**
+ * Created by weilei on 15/3/1.
+ */
+public class TestHelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
